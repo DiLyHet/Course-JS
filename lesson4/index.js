ef3519179b44e050296f85d6fe60f5797f7c4a65
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-07-03 17:45:49 +0300

    note

--- lesson4/index.js
+++ lesson4/index.js
@@ -3,7 +3,7 @@ let result = 0;
 for (let i = 1; i <= 5; ) {
     for (let k = 1; k <= 10; k++) {
         result = i * k;
-        console.log(i + ' * ' + k + ' = ' + result);
+        console.log(i + ' x ' + k + ' = ' + result);
     }
     i++;
     if (i !== 6) {
