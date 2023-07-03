a994a8f13cd8f5f947aef85b456e173c9b234b08
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-07-03 17:38:51 +0300

    note

---
+++ lesson4/index.js
@@ -0,0 +1,12 @@
+let result = 0;
+
+for (let i = 1; i <= 5; ) {
+    for (let k = 1; k <= 10; k++) {
+        result = i * k;
+        console.log(i + ' * ' + k + ' = ' + result);
+    }
+    i++;
+    if (i !== 6) {
+    console.log('=> ' + i + ' <=');
+    }
+}
