aeaea3123b13fa45142fcbea2b1eae7bbc07e1da
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-07-26 17:08:37 +0300

    s

--- calculator.js
+++
@@ -1,9 +0,0 @@
-export function getSquaredArray(array) {
-    return array.map(num => num * num);
-};
-
-export function getOddNumbers(array) {
-    return array.filter(num => (num % 2 === 1));
-};
-
-export default (a, b) => a + b;
