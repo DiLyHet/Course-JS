3b52ba57747d89dd95195980b0149fc9694be91b
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-08-17 15:08:38 +0300

    21.4

---
+++ lesson_21_4/index.js
@@ -0,0 +1,19 @@
+export function getTitle() {
+    const title = document.querySelector('.title');
+    return title.textContent;
+}
+
+export function getDescription() {
+    const description = document.querySelector('.about');
+    return description.innerText;
+}
+
+export function getPlans() {
+    const plans = document.querySelector('.plans');
+    return plans.innerHTML;
+}
+
+export function getGoal() {
+    const goal = document.querySelector('.goal');
+    return goal.outerHTML;
+}
