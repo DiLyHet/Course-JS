be9424ea6a19120d9cf89dc4047befbbaaf22813
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-08-22 12:35:39 +0300

    21.9

--- lesson_21_9/index.js
+++ lesson_21_9/index.js
@@ -1,11 +1,9 @@
 export function finishList() {
     const li = document.createElement('li');
-    const ul = document.querySelector('ul');
+    const ul = document.querySelector('.list');
     const special = document.querySelector('.special');
-    // ul.prepend(li.textContent = '1');
+    ul.prepend(li.textContent = '1');
     ul.append(li.textContent = '8');
-    // special.before(li.textContent = '4');
-    // special.after(li.textContent = '6');
+    special.before(li.textContent = '4');
+    special.after(li.textContent = '6');
 }
-
-finishList();
