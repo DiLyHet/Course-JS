c5b30a0e1294090f7ece12c8b1d1eb195f61f234
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-08-22 14:11:36 +0300

    Update index.js

--- lesson_21_12/index.js
+++ lesson_21_12/index.js
@@ -1,4 +1,4 @@
 export function squaredNumbers() {
-    const element = document.querySelector('.number');
+    const element = document.querySelectorAll('.number');
     element.setAttribute('data-squared-number', element.dataset.number**2);
 }
