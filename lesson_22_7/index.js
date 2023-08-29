f6963c94543e8e05bd66a4d72512747a1c242742
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-08-29 14:09:16 +0300

    Update index.js

--- lesson_22_7/index.js
+++ lesson_22_7/index.js
@@ -1,4 +1,4 @@
 const input = document.querySelector('.text-input');
-input.addEventListener('change', function() {
+input.addEventListener('change', function(event) {
     console.log(event.target.value);
 })
