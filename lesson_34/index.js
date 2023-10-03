5c76c3ccb7fefcd9065447e5cee0299ad68665a3
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-10-03 14:26:34 +0300

    Update index.js

--- lesson_34/index.js
+++ lesson_34/index.js
@@ -1,81 +1,80 @@
 const baseUrl = 'https://651be3e0194f77f2a5af0850.mockapi.io/api/v1/users';
 
 export function getUsersList() {
-    fetch(baseUrl, {
-      method: 'GET',
-      headers: { 'content-type': 'application/json' },
+    return fetch(baseUrl, {
+        method: 'GET',
+        headers: { 'content-type': 'application/json' },
     })
-      .then(res => {
-        return res.json();
-      })
-      .then(tasks => {})
-      .catch(error => {
-        console.error('Error');
-      });
-  }
-  
-  export function getUserById(userId) {
-    fetch(`${baseUrl}/${userId}`, {
-      method: 'GET',
-      headers: { 'content-type': 'application/json' },
+        .then(res => {
+            return res.json();
+        })
+        .then(tasks => { })
+        .catch(error => {
+            console.error('Error');
+        });
+}
+
+export function getUserById(userId) {
+    return fetch(`${baseUrl}/${userId}`, {
+        method: 'GET',
+        headers: { 'content-type': 'application/json' },
     })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then(tasks => {})
-      .catch(error => {
-        console.error('Error');
-      });
-  }
-  
-  export function createUser(userData) {
-    fetch(baseUrl, {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify(userData),
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(tasks => { })
+        .catch(error => {
+            console.error('Error');
+        });
+}
+
+export function createUser(userData) {
+    return fetch(baseUrl, {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(userData),
     })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then(task => {})
-      .catch(error => {
-        console.error('Error');
-      });
-  }
-  
-  export function deleteUser(userId) {
-    fetch(`${baseUrl}/${userId}`, {
-      method: 'DELETE',
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(task => { })
+        .catch(error => {
+            console.error('Error');
+        });
+}
+
+export function deleteUser(userId) {
+    return fetch(`${baseUrl}/${userId}`, {
+        method: 'DELETE',
     })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then(task => {})
-      .catch(error => {
-        console.error('Error');
-      });
-  }
-  
-  export function updateUser(userId, userData) {
-    fetch(`${baseUrl}/${userId}`, {
-      method: 'PUT',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify(userData),
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(task => { })
+        .catch(error => {
+            console.error('Error');
+        });
+}
+
+export function updateUser(userId, userData) {
+    return fetch(`${baseUrl}/${userId}`, {
+        method: 'PUT',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(userData),
     })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-      })
-      .then(task => {})
-      .catch(error => {
-        console.error('Error');
-      });
-  }
-  
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .then(task => { })
+        .catch(error => {
+            console.error('Error');
+        });
+}
