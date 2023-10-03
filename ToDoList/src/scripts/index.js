57265312996211456399004c11babddab941e07f
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-10-03 13:04:18 +0300

    Update index.js

--- ToDoList/src/scripts/index.js
+++ ToDoList/src/scripts/index.js
@@ -6,7 +6,7 @@ export const listElem = document.querySelector('.list');
 export const inputElem = document.querySelector('.task-input');
 export const btnElem = document.querySelector('.create-task-btn');
 
-export const apiEndPoint = "https://651a9554340309952f0d8d60.mockapi.io/api/v1/Item";
+export const apiEndPoint = "https://651be3e0194f77f2a5af0850.mockapi.io/api/v1/Item";
 export let idCounter = tasks.length + 1;
 export function addIdCounter(){
   idCounter++;
