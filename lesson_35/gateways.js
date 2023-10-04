33e3f13a00a148d60e33e32a3cf09feae90d423d
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-10-04 17:41:05 +0300

    Update gateways.js

--- lesson_35/gateways.js
+++ lesson_35/gateways.js
@@ -1,6 +1,11 @@
 export const fetchUserData = userName => {
     return fetch(`https://api.github.com/users/${userName}`)
-        .then(response => response.json());
+        .then(response => {
+            if (response.status === 200) {
+                return response.json()
+            }
+            throw new Error('Failed to load data')
+        });
 };
 
 export const fetchRepositories = url =>
