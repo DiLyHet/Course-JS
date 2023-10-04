82e85005af13c2863af25ef39a20b2c72397fc42
DiLyHet <131236310+DiLyHet@users.noreply.github.com>
2023-10-04 14:16:52 +0300

    Update index.js

--- RegistrationForm/index.js
+++ RegistrationForm/index.js
@@ -20,7 +20,7 @@ function registration(userData) {
         }).then(res => {
             console.log(JSON.stringify(userData));
             if (res.ok) {
-                res.json().then(result => alert(JSON.stringify(result)));
+                res.json().then(result => alert(result));
                 form.reset();
                 checkValidation();
             } else {
